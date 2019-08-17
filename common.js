585343e8bcaa4208518589f530885b33d9781eac
JB Doors <kim@jbdoors.co.uk>
2019-08-18 00:45:07 +0100

    Added basic debounced live update functionality - no persistence yet

--- common.js
+++ common.js
@@ -1,6 +1,10 @@
 const assert = require('assert');
 const axios = require('axios');
 const moment = require('moment');
+const _ = require('lodash');
+const logger = require('logdown')('common');
+
+logger.state.isEnabled = true;
 
 module.exports = () => {
   const baseUrl = (username, password, apiKey) => {
@@ -28,12 +32,14 @@ module.exports = () => {
       assert(end instanceof moment, 'end date must be instance of moment');
 
       const response = await axios.get(
-        `${baseUrl(username, password, apiKey)}&action=jobs&start=${start.format('YYYY-MM-DD')}&end=${end.format('YYYY-MM-DD')}`,
+        `${baseUrl(username, password, apiKey)}&action=jobs&start=${start.format('YYYY-MM-DD')}&end=${end.format('YYYY-MM-DD HH:mm:ss')}&unallocated=false`,
       );
 
       if (requestSuccess(response)) {
+        logger.info('Get Jobs Request Succeeded');
         result.data = response.data.Result;
       } else {
+        logger.error('Get Jobs Request Failed');
         result.error = response.error;
       }
     } catch (err) {
@@ -52,9 +58,10 @@ module.exports = () => {
     try {
       assert(jobId, 'jobId must not be empty');
 
-      const response = await axios.get(
+
+      const response = _.debounce(await axios.get(
         `${baseUrl(username, password, apiKey)}&action=job&jobid=${jobId}&flaghistory=1`,
-      );
+      ), 5000);
 
       if (requestSuccess(response)) {
         result.data = response.data.Result;
@@ -77,9 +84,9 @@ module.exports = () => {
     try {
       assert(jobId, 'jobId must not be empty');
 
-      const response = await axios.get(
+      const response = _.debounce(await axios.get(
         `${baseUrl(username, password, apiKey)}&action=jobworksheets&jobid=${jobId}&wsPhotos=None`,
-      );
+      ));
 
       if (requestSuccess(response)) {
         result.data = response.data.Result;
--- process/live.js
+++ process/live.js
@@ -1,10 +1,29 @@
+require('dotenv').config();
+
 const logger = require('logdown')('live');
 const moment = require('moment');
-
-const common = require('../common');
+const common = require('../common')();
 
 const start = moment().startOf('day');
 const end = moment().endOf('day');
 
 logger.state.isEnabled = true;
 logger.info(`Checking live jobs for updates between ${start.format('DD/MM/YYYY h:mm:ss')} and ${end.format('DD/MM/YYYY h:mm:ss')}`);
+
+common.getJobs(
+  process.env.bcUsername,
+  process.env.bcPassword,
+  process.env.bcApiKey,
+  start,
+  end,
+).then((results) => {
+  const { data, error } = results;
+
+  if (error) {
+    logger.error(`Error occurred: ${error}`);
+  } else {
+    logger.info(`Retrieved job data: ${data instanceof Array ? `${data.length} jobs` : 'No data'}`);
+  }
+}).catch((err) => {
+  logger.error(`Error occurred: ${err}`);
+});
