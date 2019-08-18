bc04ca035c85fdaf099b1c00d5bb2a2f7419c5e5
JB Doors <kim@jbdoors.co.uk>
2019-08-18 01:10:34 +0100

    Added keepalive setTimeout script to stop Heroku constantly restarting app.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const logger = require('logdown')('index');
 
-logger.info('Scheduled jobs platform running');
+logger.state.isEnabled = true;
+
+const aliveMessage = () => {
+  logger.info('Scheduled jobs platform running');
+  setTimeout(aliveMessage, 1000 * 60);
+};
+
+aliveMessage();
 
 process.on('SIGINT', (signal) => {
   logger.info(`${signal} - Scheduled jobs platform stopping`);
